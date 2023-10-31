586c0a63cee52b3a28d322212613f2bab905f4e6
Madhavi Swamy <madhavi.swamy@cumminscollege.in>
2023-10-31 15:18:08 +0000

    Implementing LSP and create relevant Bird classes

--- src/main/java/birds/Bird.java
+++ src/main/java/birds/Bird.java
@@ -4,8 +4,4 @@ public class Bird {
     public void eat() {
         System.out.println("Bird is eating.");
     }
-
-    public void fly() {
-        System.out.println("Bird is flying.");
-    }
 }
---
+++ src/main/java/discountStrategy/DiscountStratergy.java
@@ -0,0 +1,2 @@
+package discountStrategy;public interface DiscountStratergy {
+}
